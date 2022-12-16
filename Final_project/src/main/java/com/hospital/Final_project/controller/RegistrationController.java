a7b80761f354b2aa2217f453826a59e28b8318a2
lux <ssbekmuratov@gmail.com>
2022-12-16 11:43:51 -0600

    registration

--- Final_project/src/main/java/com/hospital/Final_project/controller/RegistrationController.java
+++ Final_project/src/main/java/com/hospital/Final_project/controller/RegistrationController.java
@@ -2,8 +2,12 @@ package com.hospital.Final_project.controller;
 
 import com.hospital.Final_project.user.User;
 import com.hospital.Final_project.user.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -15,10 +19,19 @@ public class RegistrationController {
         this.userService = userService;
     }
 
-    @PostMapping("/registration")
+    @GetMapping("/api/v1/registration")
+    public ResponseEntity<User> userRegistration(){
+        return new ResponseEntity(new User(), HttpStatus.OK);
+    }
+    @PostMapping("/api/v1/registration")
     public ResponseEntity<?> userRegistration(@RequestBody User user){
         userService.save(user);
-        return ResponseEntity.ok(new String("User registered successfully!"));
+        return ResponseEntity.ok("User registered successfully!");
+    }
+
+    @PostMapping("/api/v1/login")
+    public ResponseEntity<?> login(){
+        return ResponseEntity.ok("Login page");
     }
 
 }
