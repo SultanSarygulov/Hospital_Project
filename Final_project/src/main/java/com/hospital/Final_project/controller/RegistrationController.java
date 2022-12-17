26d3ad93cd35cb733867120de7eb2a2b9d7b8b1d
lux <ssbekmuratov@gmail.com>
2022-12-17 01:27:28 -0600

    login method

--- Final_project/src/main/java/com/hospital/Final_project/controller/RegistrationController.java
+++ Final_project/src/main/java/com/hospital/Final_project/controller/RegistrationController.java
@@ -1,37 +1,49 @@
 package com.hospital.Final_project.controller;
 
-import com.hospital.Final_project.user.User;
+import com.hospital.Final_project.user.UserDTO;
 import com.hospital.Final_project.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
-import java.util.Collections;
+
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping(path = "/api/v1")
 public class RegistrationController {
 
-    UserService userService;
+    private UserService userService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     public RegistrationController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/api/v1/registration")
-    public ResponseEntity<User> userRegistration(){
-        return new ResponseEntity(new User(), HttpStatus.OK);
+    @GetMapping
+    public String userRegistration(){
+        return "register";
     }
-    @PostMapping("/api/v1/registration")
-    public ResponseEntity<?> userRegistration(@RequestBody User user){
-        userService.save(user);
+    @PostMapping
+    public ResponseEntity<?> userRegistration(@RequestBody UserDTO userDTO){
+        userDTO.setRole("CLIENT");
+        userService.save(userDTO);
         return ResponseEntity.ok("User registered successfully!");
     }
 
-    @PostMapping("/api/v1/login")
-    public ResponseEntity<?> login(){
-        return ResponseEntity.ok("Login page");
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody UserDTO userDTO){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                userDTO.getEmail(), userDTO.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return new ResponseEntity<>("User signed-in successfully!.", HttpStatus.OK);
     }
+
 
 }
--- Final_project/src/main/java/com/hospital/Final_project/user/UserService.java
+++ Final_project/src/main/java/com/hospital/Final_project/user/UserService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface UserService extends UserDetailsService {
-    User save(User user);
+    User save(UserDTO userDTO);
 
     User findByEmail(String email);
 
